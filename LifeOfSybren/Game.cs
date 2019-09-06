f7a0e2747e036322ba2768cbb66219cbbe27e44e
Ossama Sybesma <ossama_sybesma@hotmail.com>
2019-09-06 14:19:01 +0200

    Game changes

---
+++ LifeOfSybren/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeOfSybren
+{
+    public class Game
+    {
+        private static readonly Game instance = new Game();
+        private string choice;
+
+        private Game()
+        {
+
+        }
+
+        public static Game Instance => instance;
+
+        public void ActOne()
+        {
+            Console.WriteLine("You are finally awake");
+            Console.WriteLine("1. Sumthing");
+            Console.WriteLine("2. Nogwat");
+            Console.WriteLine("3. Ding");
+            choice = Console.ReadLine().ToLower();
+            Console.Clear();
+        }
+    }
+}
